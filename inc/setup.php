fb2812f540872ce59d50ed482204a08bdf07ad73
Nabil Kadimi <nabil@kadimi.com>
2019-11-11 06:05:14 +0100

    Remove Piklist

--- inc/setup.php
+++ inc/setup.php
@@ -1,20 +1,17 @@
 <?php
 
 /**
- * Require dependencies.
+ * Register post types and taxonomies.
  */
-classifieds_by_plugible()->require_plugin( 'piklist' );
-
-/**
- * Register post types.
- */
-add_filter( 'piklist_post_types', function ( $post_types ) {
+add_action( 'init', function() {
 
 	/**
 	 * Register the "classified" post type.
 	 */
-	$post_types[ 'pl_classified' ] = [
-		'labels' => piklist( 'post_type_labels', __( 'Classifieds', 'classifieds-by-plugibles' ) ),
+	register_post_type( 'pl_classified', [
+		'labels' => [
+			'name' => 'Classifieds', 'classifieds-by-plugibles',
+		],
 		'menu_icon' => 'dashicons-megaphone',
 		'public' => true,
 			'rewrite' => [
@@ -25,53 +22,33 @@ add_filter( 'piklist_post_types', function ( $post_types ) {
 			'editor',
 			'title',
 		],
-	];
-
-	return $post_types;
-} );
-
-/**
- * Register taxonomies. 
- */
-add_filter('piklist_taxonomies', function ($taxonomies) {
+	] );
 
 	/**
 	 * Register the "Classified/Location" taxonomy.
 	 */
-	$taxonomies[] = [
-		'post_type' => 'pl_classified',
-		'name' => 'pl_classified_location',
-		'show_admin_column' => true,
-		'configuration' => [
-			'hierarchical' => true,
-			'labels' => piklist( 'taxonomy_labels', __( 'Location', 'classifieds-by-plugibles' ) ),
-			'hide_meta_box' => true,
-			'show_ui' => true,
-			'query_var' => true,
-			'rewrite' => [
-				'slug' => 'classifieds-location',
-			],
+	register_taxonomy( 'pl_classified_location', [ 'pl_classified' ], [
+		'labels' => [
+			'name' => 'Locations', 'classifieds-by-plugibles',
 		],
-	];
+		'show_admin_column' => true,
+		'hierarchical' => true,
+		'rewrite' => [
+			'slug' => 'classifieds-location',
+		],
+	] );
 
 	/**
 	 * Register the "Classified/Category" taxonomy.
 	 */
-	$taxonomies[] = [
-		'post_type' => 'pl_classified',
-		'name' => 'pl_classified_category',
-		'show_admin_column' => true,
-		'configuration' => [
-			'hierarchical' => true,
-			'labels' => piklist( 'taxonomy_labels', __( 'Category', 'classifieds-by-plugibles' ) ),
-			'hide_meta_box' => true,
-			'show_ui' => true,
-			'query_var' => true,
-			'rewrite' => [
-				'slug' => 'classifieds-category',
-			],
+	register_taxonomy( 'pl_classified_category', [ 'pl_classified' ], [
+		'labels' => [
+			'name' => 'Categories', 'classifieds-by-plugibles',
 		],
-	];
-
-	return $taxonomies;
+		'show_admin_column' => true,
+		'hierarchical' => true,
+		'rewrite' => [
+			'slug' => 'classifieds-category',
+		],
+	] );
 } );
