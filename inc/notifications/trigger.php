235a8eb4c20171fcb8b3b0050726b14eb0118597
Nabil Kadimi <nabil@kadimi.com>
2020-04-05 00:23:58 +0100

    Add triggers

--- inc/notifications/trigger.php
+++ inc/notifications/trigger.php
@@ -1,21 +1,45 @@
 <?php
 
 /**
- * Trigger classified emails with new submissin.
+ * Classified submitted.
  */
-add_action( 'plcl_classified_inserted', function( $post_id ) {
-	$post = get_post( $post_id );
-	do_action( 'plcl_classified_inserted_' . $post->post_status, $post_id );
+add_action( 'plcl_classified_inserted_publish', function( $post_id ) {
+	do_action( 'plcl_classified_approved', $post_id );
+} );
+add_action( 'plcl_classified_inserted_draft', function( $post_id ) {
+	do_action( 'plcl_classified_pending', $post_id );
 } );
 
 /**
- * Trigger classified emails with status change.
+ * Classified status changed.
  */
 add_action( 'transition_post_status', function( $new_status, $old_status, $post ) {
-	if ( 'pl_classified' === $post->post_type
-		&& 'publish' === $new_status
-		&& 'draft' === $old_status
-	) {
-		plcl_mail( 'ad_approved', $post->ID );
+	if ( $old_status !== $new_status && 'pl_classified' === $post->post_type ) {
+		switch ( $new_status ) {
+		case 'publish':
+			do_action( 'plcl_classified_approved', $post->ID );
+			break;
+		case 'trash':
+			do_action( 'plcl_classified_rejected', $post->ID );
+			break;
+		default:
+			break;
+		}
 	}
 }, 10, 3 );
+
+/**
+ * Classified comment approved.
+ */
+add_action( 'transition_comment_status', function( $new_status, $old_status, $comment ) {
+	if ( $old_status !== $new_status && 'pl_classified' === get_post_type( $comment->comment_post_ID ) ) {
+		if ( 'approved' === $new_status ) {
+			do_action( 'plcl_comment_approved', $comment );
+		}
+	}
+}, 10, 3 );
+add_action( 'wp_insert_comment', function( $id, $comment ) {
+	if ( 'pl_classified' === get_post_type( $comment->comment_post_ID ) ) {
+		do_action( 'plcl_comment_approved', $comment );
+	}
+}, 10, 2 );
