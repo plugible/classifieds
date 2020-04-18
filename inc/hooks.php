aea57786867f21ff805c2abc18828f97e4049455
Nabil Kadimi <nabil@kadimi.com>
2020-04-18 15:29:35 +0100

    Improve hooks

--- inc/hooks.php
+++ inc/hooks.php
@@ -6,17 +6,26 @@
  * - Classified published
  * - Classified created with status `publish`
  */
-add_action( 'plcl_classified_inserted_publish', function( $post_id ) {
-	do_action( 'plcl_classified_approved', $post_id );
+add_action( 'init', function() {
+	static $once = [];
+	add_action( 'plcl_classified_inserted_publish', function( $post_id ) use ( &$once ) {
+		if ( ! in_array( $post_id, $once ) ) {
+			$once[] = $post_id;
+			do_action( 'plcl_classified_approved', $post_id );
+		}
+	} );
+	add_action( 'transition_post_status', function( $new_status, $old_status, $post ) use ( &$once ) {
+		if ( ! in_array( $post->ID, $once ) ) {
+			$once[] = $post->ID;
+			if ( $old_status !== $new_status
+				&& 'pl_classified' === $post->post_type
+				&& 'publish' === $new_status
+			) {
+				do_action( 'plcl_classified_approved', $post->ID );
+			}
+		}
+	}, 10, 3 );
 } );
-add_action( 'transition_post_status', function( $new_status, $old_status, $post ) {
-	if ( $old_status !== $new_status
-		&& 'pl_classified' === $post->post_type
-		&& 'publish' === $new_status
-	) {
-		do_action( 'plcl_classified_approved', $post->ID );
-	}
-}, 10, 3 );
 
 /**
  * plcl_classified_pending
@@ -48,19 +57,40 @@ add_action( 'transition_post_status', function( $new_status, $old_status, $post 
  * - pl_classified comment approved
  * - pl_classified comment inserted with status `approve`
  */
-add_action( 'transition_comment_status', function( $new_status, $old_status, $comment ) {
-	if ( $old_status !== $new_status
-		&& 'pl_classified' === get_post_type( $comment->comment_post_ID )
-		&& 'approved' === $new_status
-	) {
-		do_action( 'plcl_comment_approved', $comment->comment_ID );
+add_action( 'init', function() {
+	static $once = [];	
+	add_action( 'transition_comment_status', function( $new_status, $old_status, $comment ) use( &$once ) {
+		if ( ! in_array( $comment->comment_ID, $once ) ) {
+			$once[] = $comment->comment_ID;
+			if ( $old_status !== $new_status
+				&& 'pl_classified' === get_post_type( $comment->comment_post_ID )
+				&& 'approved' === $new_status
+			) {
+				do_action( 'plcl_comment_approved', $comment->comment_ID );
+			}
+		}
+	}, 10, 3 );
+	add_action( 'wp_insert_comment', function( $id, $comment ) use( &$once ) {
+		if ( ! in_array( $comment->comment_ID, $once ) ) {
+			$once[] = $comment->comment_ID;
+			if ( 'pl_classified' === get_post_type( $comment->comment_post_ID ) && $comment->comment_approved) {
+				do_action( 'plcl_comment_approved', $id );
+			}
+		}
+	}, 10, 2 );
+} );
+
+/**
+ * plcl_comment_rejected
+ *
+ * - pl_classified comment rejected
+ * - pl_classified comment inserted with status `approve`
+ */
+add_action( 'comment_unapproved_to_trash', function( $comment ) {
+	if ( 'pl_classified' === get_post_type( $comment->comment_post_ID ) ) {
+		do_action( 'plcl_comment_rejected', $comment->comment_ID );
 	}
-}, 10, 3 );
-add_action( 'wp_insert_comment', function( $id, $comment ) {
-	if ( 'pl_classified' === get_post_type( $comment->comment_post_ID ) && $comment->comment_approved) {
-		do_action( 'plcl_comment_approved', $id );
-	}
-}, 10, 2 );
+} );
 
 /**
  * plcl_comment_update_hashes
@@ -71,12 +101,40 @@ add_action( 'wp_insert_comment', function( $id, $comment ) {
  */
 add_action( 'init', function() {
 	$hook_update_hashes = function( $comment_id ) {
-		if ( 'pl_classified' === get_post_type( get_comment( $comment_id )->comment_post_ID ) ) {
-			do_action( 'plcl_comment_update_hashes', $comment_id );
+		static $once = [];
+		if ( ! in_array( $comment_id, $once ) ) {
+			$once[] = $comment_id;
+			if ( 'pl_classified' === get_post_type( get_comment( $comment_id )->comment_post_ID ) ) {
+				do_action( 'plcl_comment_update_hashes', $comment_id );
+			}
 		}
 	};
-
 	add_action( 'edit_comment', $hook_update_hashes );
 	add_action( 'wp_insert_comment', $hook_update_hashes );
 	add_action( 'wp_set_comment_status', $hook_update_hashes );
+} );
+
+/**
+ * plcl_classified_update_hashes
+ *
+ * - Classified created
+ * - Classified updated
+ * - Classified status updated
+ */
+add_action( 'init', function() {
+	$once = [];
+	add_action( 'transition_post_status', function( $_1, $_2, $post ) use( &$once ) {
+		if ( 'pl_classified' === $post->post_type ) {
+			if ( ! in_array( $post->ID, $once ) ) {
+				$once[] = $post->ID;
+				do_action( 'plcl_classified_update_hashes', $post->ID );
+			}
+		};
+	}, 9, 3 );
+	add_action( 'save_post_pl_classified', function( $post_id ) use( &$once ) {
+		if ( ! in_array( $post_id, $once ) ) {
+			$once[] = $post_id;
+			do_action( 'plcl_classified_update_hashes', $post_id );
+		}
+	}, 9 );
 } );
