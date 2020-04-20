5c7423bb4c58174fb9dc92a1484ab5c8d77faa6a
Nabil Kadimi <nabil@kadimi.com>
2020-04-20 17:38:50 +0000

    Improve debug mode

--- inc/debug/meta.php
+++ inc/debug/meta.php
@@ -5,7 +5,7 @@ if ( ! defined( 'WP_DEBUG' ) || ! WP_DEBUG ) {
 }
 
 /**
- * Show Classifieds meta.
+ * Show Classifieds meta in admin table.
  */
 add_action( 'manage_pl_classified_posts_custom_column', function( $column_name, $post_id ) {
 	if ( 'meta' == $column_name ) {
@@ -15,7 +15,12 @@ add_action( 'manage_pl_classified_posts_custom_column', function( $column_name, 
 				|| 'comment_hash' === substr( $meta_key, 0, 12 )
 			;
 		}, ARRAY_FILTER_USE_KEY );
-		echo '<pre>' . json_encode( $meta,  JSON_PRETTY_PRINT ) . '</pre>';
+		foreach ( $meta as $k => $v) {
+			echo "$k:<br>";
+			foreach ( $v as $vv) {
+				echo "- $vv<br>";
+			}
+		}
 	}
 }, 10, 2 );
 add_filter( 'manage_pl_classified_posts_columns', function ( $columns ) {
@@ -24,16 +29,41 @@ add_filter( 'manage_pl_classified_posts_columns', function ( $columns ) {
 } );
 
 /**
- * Show comment meta.
+ * Show comment meta in admin table.
  */
 add_filter( 'manage_edit-comments_columns', function( $columns ) {
 	return array_merge( $columns, [ 'meta' => __( 'Meta', 'classifieds-by-plugible' ) ] );
 } );
 add_filter( 'manage_comments_custom_column', function( $column, $comment_id ) {
 	if ( 'meta' === $column ) {
-		$meta = array_filter( get_comment_meta( $comment_id ), function( $meta_key ) {
-			return 'comment_hash' === substr( $meta_key, 0, 12 );
+		$meta = array_filter( get_comment_meta( $comment_id ), function( $k ) {
+			return 'comment_hash' === substr( $k, 0, 12 );
 		}, ARRAY_FILTER_USE_KEY );
-		echo '<pre>' . json_encode( $meta,  JSON_PRETTY_PRINT ) . '</pre>';
+		foreach ( $meta as $k => $v) {
+			echo "$k:<br>";
+			foreach ( $v as $vv) {
+				echo "- $vv<br>";
+			}
+		}
 	}
 }, 10, 2 );
+
+/**
+ * Show comment meta in frontend.
+ */
+add_filter( 'comment_text', function( $comment_text, $comment, $args ) {
+	if ( !is_admin() && 'pl_classified' === get_post_type( $comment->comment_post_ID ) ) {
+		$meta = array_filter( get_comment_meta( $comment->comment_ID ), function( $k ) {
+			return 'comment_hash' === substr( $k, 0, 12 );
+		}, ARRAY_FILTER_USE_KEY );
+		$add_text = '';
+		foreach ( $meta as $k => $v) {
+			$add_text .= "$k:\n";
+			foreach ( $v as $vv) {
+				$add_text .= "- $vv\n";
+			}
+		}
+		$comment_text .= "<pre>$add_text</pre>";
+	}
+	return $comment_text;
+}, 10, 3 );
