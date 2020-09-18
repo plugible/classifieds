c13ad5cd8698b0f7bc67a084882ed5466b71fe86
Nabil Kadimi <nabil@kadimi.com>
2020-09-18 10:36:55 +0100

    Add constants

--- bootstrap.php
+++ bootstrap.php
@@ -13,6 +13,12 @@
  */
 
 /**
+ * Configuration.
+ */
+define( 'PLCL_ADS_PER_PAGE', apply_filters( 'plcl_ads_per_page', 8 ) );
+define( 'PLCL_INBOX_MESSAGES_PER_PAGE', apply_filters( 'plcl_inbox_messages_per_page', 1 ) );
+
+/**
  * Composer stuff.
  */
 require 'vendor/autoload.php';
