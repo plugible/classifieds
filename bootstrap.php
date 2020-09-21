d65b495a120cac8f8f9f42c83f5dcabc0dd86480
Nabil Kadimi <nabil@kadimi.com>
2020-09-21 01:33:59 +0100

    Minor

--- bootstrap.php
+++ bootstrap.php
@@ -14,8 +14,8 @@
 /**
  * Configuration.
  */
-define( 'PLCL_ADS_PER_PAGE', apply_filters( 'plcl_ads_per_page', 8 ) );
-define( 'PLCL_INBOX_MESSAGES_PER_PAGE', apply_filters( 'plcl_inbox_messages_per_page', 1 ) );
+define( 'PLCL_ADS_PER_PAGE', apply_filters( 'plcl_ads_per_page', 20 ) );
+define( 'PLCL_INBOX_MESSAGES_PER_PAGE', apply_filters( 'plcl_inbox_messages_per_page', 50 ) );
 
 /**
  * Composer stuff.
