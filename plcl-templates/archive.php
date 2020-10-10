e5557bec25a4b3b70ed61817d6d14608b94fb194
Nabil Kadimi <nabil@kadimi.com>
2020-10-10 05:10:32 +0100

    Fix archive

--- plcl-templates/archive.php
+++ plcl-templates/archive.php
@@ -2,6 +2,6 @@
 
 <?php plcl_classified_gallery( $post->ID, 1 ); ?>
 
-<?php echo $post->post_content; ?>
+<?php echo wpautop( $post->post_content ); ?>
 
 <?php plcl_classified_specs( $post->ID ); ?>
