5cbb84fc55ade7582367196f12eb74b005514e25
Nabil Kadimi <nabil@kadimi.com>
2020-10-20 04:22:14 +0100

    Add catefory ancestors to breadcrumbs

--- inc/functions.php
+++ inc/functions.php
@@ -272,6 +272,17 @@ function plcl_breadcrumbs( $open, $close ) {
 		$filters = $_REQUEST[ 'filters' ] ?? [];
 
 		/**
+		 * Ancestors.
+		 */
+		$category_ancestors  = get_ancestors( plcl_get_the_category()->term_id, 'pl_classified_category', 'taxonomy' );
+		array_walk( $category_ancestors, function( $ancestor_id  ) use ( &$paths ) {
+			$paths[] = [
+				'text' => get_term( $ancestor_id )->name,
+				'url'  => get_term_link( $ancestor_id, 'pl_classified_category' ),
+			];
+		} );
+
+		/**
 		 * Page 1.
 		 */
 		$paths[] = [
