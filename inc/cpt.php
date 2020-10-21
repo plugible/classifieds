adf4e0ceb6c17fc510b283d71c604c11ef862fec
Nabil Kadimi <nabil@kadimi.com>
2020-10-21 22:02:55 +0100

    Minor

--- inc/cpt.php
+++ inc/cpt.php
@@ -227,7 +227,6 @@ add_action( 'cmb2_admin_init', function() {
  */
 add_action( 'before_delete_post', function( $post_id ) {
 
-
     global $post_type;
 	if ( 'pl_classified' !== $post_type ) {
 		return;
@@ -235,6 +234,31 @@ add_action( 'before_delete_post', function( $post_id ) {
 	foreach ( get_attached_media( '', $post_id ) as $attachment ) {
 		wp_delete_attachment( $attachment->ID );
 	}
+} );
+
+/**
+ * Add attachment to CMB2's `images` field. 
+ */
+add_action("add_attachment", function( $attachment_id ) {
+
+	$attachment_post = get_post( $attachment_id );
+
+	if ( ! $attachment_post->post_parent ) {
+		return;
+	}
+
+	$parent_post = get_post( $attachment_post->post_parent );
+	if ( 'pl_classified' !== $parent_post->post_type ) {
+		return;
+	}
+
+	$attachments = get_posts( [
+		'post_type'   => 'attachment',
+		'post_parent' => $parent_post->ID,
+	] );
+
+	delete_post_meta( $parent_post->ID, 'images' );
+	add_post_meta( $parent_post->ID, 'images', wp_list_pluck( $attachments, 'guid', 'ID' ) );
 } );
 
 /**
