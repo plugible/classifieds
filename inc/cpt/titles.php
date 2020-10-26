43424b85ccc28adb1cad8adc3cc2b9b8c8f3ee19
Nabil Kadimi <nabil@kadimi.com>
2020-10-26 08:56:11 +0100

    Customize document titles for ads and ad categories

---
+++ inc/cpt/titles.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Ad.
+ */
+add_action( 'single_post_title', function( $title ) {
+	if ( is_singular( 'pl_classified' ) ) {
+		if ( plcl_get_the_category() ) {
+			$title .= ' ‹ ' . plcl_get_the_category()->name;
+		}
+	}
+	return $title;
+} );
+
+/**
+ * Category.
+ */
+add_action( 'single_term_title', function( $title ) {
+
+	if ( is_archive() && is_tax( 'pl_classified_category' ) ) {
+
+		/**
+		 * Category ancestors.
+		 */
+		$category_ancestors  = get_ancestors( plcl_get_the_category()->term_id, 'pl_classified_category', 'taxonomy' );
+		array_walk( $category_ancestors, function( $ancestor_id  ) use ( &$title ) {
+			$title .= ' ‹ ' . get_term( $ancestor_id )->name ;
+		} );
+	}
+	return $title;
+} );
